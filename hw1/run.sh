81f999827daf499649323ad5d8d634d225fc060d
b10902069 <b10902069@meow1.csie.ntu.edu.tw>
2023-10-06 01:50:10 +0800

    main structure finished

--- hw1/run.sh
+++ hw1/run.sh
@@ -6,7 +6,7 @@ export PYTHONPATH
 CUDA_VISIBLE_DEVICES=1 python3 ./src/main.py \
 --test_file /tmp2/b10902069/adl_hw1/test.json \
 --context_file /tmp2/b10902069/adl_hw1/context.json \
---max_seq_length 128 \
+--max_seq_length 384 \
 --model_name_or_path_multiple_choice /tmp2/b10902069/adl_hw1/multiple_choice_dir/ \
 --model_name_or_path_extractive /tmp2/b10902069/adl_hw1/extractive_dir/ \
 --output_dir /tmp2/b10902069/adl_hw1 \
@@ -15,3 +15,11 @@ CUDA_VISIBLE_DEVICES=1 python3 ./src/main.py \
 # --with_tracking
 # --gradient_accumulation_steps
 # --pad_to_max_length
+# --preprocessing_num_workers
+# --overwrite_cache
+# --per_device_eval_batch_size
+# --version_2_with_negative
+# --max_answer_length
+# --null_score_diff_threshold
+# --doc_stride
+# --n_best_size
--- hw1/src/main.py
+++ hw1/src/main.py
@@ -94,6 +94,54 @@ def parse_args():
             " sequences shorter will be padded if `--pad_to_max_lengh` is passed."
         ),
     )
+    parser.add_argument(
+        "--preprocessing_num_workers", type=int, default=1, help="A csv or a json file containing the training data."
+    )
+    parser.add_argument(
+        "--overwrite_cache", action="store_true", help="Overwrite the cached training and evaluation sets"
+    )
+    parser.add_argument(
+        "--per_device_eval_batch_size",
+        type=int,
+        default=8,
+        help="Batch size (per device) for the evaluation dataloader.",
+    )
+    parser.add_argument(
+        "--version_2_with_negative",
+        action="store_true",
+        help="If true, some of the examples do not have an answer.",
+    )
+    parser.add_argument(
+        "--max_answer_length",
+        type=int,
+        default=30,
+        help=(
+            "The maximum length of an answer that can be generated. This is needed because the start "
+            "and end predictions are not conditioned on one another."
+        ),
+    )
+    parser.add_argument(
+        "--null_score_diff_threshold",
+        type=float,
+        default=0.0,
+        help=(
+            "The threshold used to select the null answer: if the best answer has a score that is less than "
+            "the score of the null answer minus this threshold, the null answer is selected for this example. "
+            "Only useful when `version_2_with_negative=True`."
+        ),
+    )
+    parser.add_argument(
+        "--doc_stride",
+        type=int,
+        default=128,
+        help="When splitting up a long document into chunks how much stride to take between chunks.",
+    )
+    parser.add_argument(
+        "--n_best_size",
+        type=int,
+        default=20,
+        help="The total number of n-best predictions to generate when looking for an answer.",
+    )
     args = parser.parse_args()
 
     return args
@@ -197,7 +245,7 @@ def main():
             pretrained_model_name_or_path=args.model_name_or_path_extractive,
             cache_dir=args.output_dir
         )
-        tokenizer_e = AutoConfig.from_pretrained(
+        tokenizer_e = AutoTokenizer.from_pretrained(
             pretrained_model_name_or_path=args.model_name_or_path_extractive,
             use_fast=True,
             cache_dir=args.output_dir
@@ -236,7 +284,7 @@ def main():
     # First we tokenize all the texts.
     padding = "max_length" if args.pad_to_max_length else False
 
-    def preprocess_function(examples):
+    def preprocess_function_m(examples):
         first_sentences = [[question] * 4 for question in examples[question_name]]
         second_sentences = [[context[i] for i in context_ids] for context_ids in examples[context_name]]
 
@@ -259,7 +307,7 @@ def main():
 
     with accelerator.main_process_first():
         processed_datasets = raw_datasets.map(
-            preprocess_function, batched=True, remove_columns=raw_datasets["test"].column_names
+            preprocess_function_m, batched=True, remove_columns=raw_datasets["test"].column_names
         )
 
     test_dataset = processed_datasets["test"]
@@ -279,13 +327,194 @@ def main():
         with torch.no_grad():
             outputs = model_m(**batch)
         predictions = outputs.logits.argmax(dim=-1)
-        first_model_output.append(context[paragraph_list[step][predictions.item()]])
+        first_model_output.append(paragraph_list[step][predictions.item()])
         
     # Second Model -- Extractive
-    
-    model_e,  = accelerator.prepare(model_e, )
+    # Preprocessing the datasets.
+    # Preprocessing is slighlty different for training and evaluation.
+    # Padding side determines if we do (question|context) or (context|question).
+    pad_on_right = tokenizer_e.padding_side == "right"
+
+    if args.max_seq_length > tokenizer_e.model_max_length:
+        logger.warning(
+            f"The max_seq_length passed ({args.max_seq_length}) is larger than the maximum length for the"
+            f"model ({tokenizer_e.model_max_length}). Using max_seq_length={tokenizer_e.model_max_length}."
+        )
+
+    max_seq_length = min(args.max_seq_length, tokenizer_e.model_max_length)
+
+    question_column_name = "question"
+    context_column_name = "relevant"
+    # Pre-processing
+    def preprocess_function_e(examples):
+        # Some of the questions have lots of whitespace on the left, which is not useful and will make the
+        # truncation of the context fail (the tokenized question will take a lots of space). So we remove that
+        # left whitespace
+        examples[question_column_name] = [q.lstrip() for q in examples[question_column_name]]
+
+        # Tokenize our examples with truncation and maybe padding, but keep the overflows using a stride. This results
+        # in one example possible giving several features when a context is long, each of those features having a
+        # context that overlaps a bit the context of the previous feature.
+        tokenized_examples = tokenizer_e(
+            examples[question_column_name],
+            [context[i] for i in examples[context_column_name]],
+            truncation="only_second",
+            max_length=max_seq_length,
+            stride=args.doc_stride,
+            return_overflowing_tokens=True,
+            return_offsets_mapping=True,
+            padding="max_length" if args.pad_to_max_length else False,
+        ) if pad_on_right else tokenizer_e(
+            [context[i] for i in examples[context_column_name]],
+            examples[question_column_name],
+            truncation="only_first",
+            max_length=max_seq_length,
+            stride=args.doc_stride,
+            return_overflowing_tokens=True,
+            return_offsets_mapping=True,
+            padding="max_length" if args.pad_to_max_length else False,
+        )
+
+        # Since one example might give us several features if it has a long context, we need a map from a feature to
+        # its corresponding example. This key gives us just that.
+        sample_mapping = tokenized_examples.pop("overflow_to_sample_mapping")
+
+        # For evaluation, we will need to convert our predictions to substrings of the context, so we keep the
+        # corresponding example_id and we will store the offset mappings.
+        tokenized_examples["example_id"] = []
+
+        for i in range(len(tokenized_examples["input_ids"])):
+            # Grab the sequence corresponding to that example (to know what is the context and what is the question).
+            sequence_ids = tokenized_examples.sequence_ids(i)
+            context_index = 1 if pad_on_right else 0
+
+            # One example can give several spans, this is the index of the example containing this span of text.
+            sample_index = sample_mapping[i]
+            tokenized_examples["example_id"].append(examples["id"][sample_index])
+
+            # Set to None the offset_mapping that are not part of the context so it's easy to determine if a token
+            # position is part of the context or not.
+            tokenized_examples["offset_mapping"][i] = [
+                (o if sequence_ids[k] == context_index else None)
+                for k, o in enumerate(tokenized_examples["offset_mapping"][i])
+            ]
+        return tokenized_examples
+
+    eval_examples = raw_datasets["test"]
+    eval_examples = eval_examples.remove_columns("paragraphs")
+    eval_examples = eval_examples.add_column(name="relevant", column=first_model_output)
+
+    with accelerator.main_process_first():
+        eval_dataset = eval_examples.map(
+            preprocess_function_e,
+            batched=True,
+            num_proc=args.preprocessing_num_workers,
+            remove_columns=eval_examples.column_names,
+            load_from_cache_file=not args.overwrite_cache,
+            desc="Running tokenizer on test dataset",
+        )
+
+    data_collator = DataCollatorWithPadding(tokenizer_e, pad_to_multiple_of=(8 if accelerator.use_fp16 else None))
+    eval_dataset_for_model = eval_dataset.remove_columns(["example_id", "offset_mapping"])
+    eval_dataloader = DataLoader(
+        eval_dataset_for_model, collate_fn=data_collator, batch_size=args.per_device_eval_batch_size
+    )
+
+    # Post-processing:
+    def post_processing_function_e(examples, features, predictions, stage="eval"):
+        # Post-processing: we match the start logits and end logits to answers in the original context.
+        predictions = postprocess_qa_predictions(
+            examples=examples,
+            context_file=context, # the context.json is converted into a list here, called context. In utils_qa.py, it's called context_file
+            features=features,
+            predictions=predictions,
+            version_2_with_negative=args.version_2_with_negative,
+            n_best_size=args.n_best_size,
+            max_answer_length=args.max_answer_length,
+            null_score_diff_threshold=args.null_score_diff_threshold,
+            output_dir=args.output_dir,
+            prefix=stage,
+        )
+        # Format the result to the format the metric expects.
+        if args.version_2_with_negative:
+            formatted_predictions = [
+                {"id": k, "prediction_text": v, "no_answer_probability": 0.0} for k, v in predictions.items()
+            ]
+        else:
+            formatted_predictions = [{"id": k, "prediction_text": v} for k, v in predictions.items()]
+        
+        # >>> predictions = [{'prediction_text': '1976', 'id': '56e10a3be3433e1400422b22'}]
+        return formatted_predictions
+
+    # Create and fill numpy array of size len_of_validation_data * max_length_of_output_tensor
+    def create_and_fill_np_array(start_or_end_logits, dataset, max_len):
+        """
+        Create and fill numpy array of size len_of_validation_data * max_length_of_output_tensor
+
+        Args:
+            start_or_end_logits(:obj:`tensor`):
+                This is the output predictions of the model. We can only enter either start or end logits.
+            eval_dataset: Evaluation dataset
+            max_len(:obj:`int`):
+                The maximum length of the output tensor. ( See the model.eval() part for more details )
+        """
+
+        step = 0
+        # create a numpy array and fill it with -100.
+        logits_concat = np.full((len(dataset), max_len), -100, dtype=np.float64)
+        # Now since we have create an array now we will populate it with the outputs gathered using accelerator.gather_for_metrics
+        for i, output_logit in enumerate(start_or_end_logits):  # populate columns
+            # We have to fill it such that we have to take the whole tensor and replace it on the newly created array
+            # And after every iteration we have to change the step
+
+            batch_size = output_logit.shape[0]
+            cols = output_logit.shape[1]
+
+            if step + batch_size < len(dataset):
+                logits_concat[step : step + batch_size, :cols] = output_logit
+            else:
+                logits_concat[step:, :cols] = output_logit[: len(dataset) - step]
+
+            step += batch_size
+
+        return logits_concat
+
+    model_e, eval_dataloader = accelerator.prepare(model_e, eval_dataloader)
     model_e.eval()
 
+    all_start_logits = []
+    all_end_logits = []
+    for step, batch in enumerate(eval_dataloader):
+        with torch.no_grad():
+            outputs = model_e(**batch)
+            start_logits = outputs.start_logits
+            end_logits = outputs.end_logits
+
+            if not args.pad_to_max_length:  # necessary to pad predictions and labels for being gathered
+                start_logits = accelerator.pad_across_processes(start_logits, dim=1, pad_index=-100)
+                end_logits = accelerator.pad_across_processes(end_logits, dim=1, pad_index=-100)
+
+            all_start_logits.append(accelerator.gather_for_metrics(start_logits).cpu().numpy())
+            all_end_logits.append(accelerator.gather_for_metrics(end_logits).cpu().numpy())
+
+    max_len = max([x.shape[1] for x in all_start_logits])  # Get the max_length of the tensor
+
+    # concatenate the numpy array
+    start_logits_concat = create_and_fill_np_array(all_start_logits, eval_dataset, max_len)
+    end_logits_concat = create_and_fill_np_array(all_end_logits, eval_dataset, max_len)
+
+    # delete the list of numpy arrays
+    del all_start_logits
+    del all_end_logits
+
+    outputs_numpy = (start_logits_concat, end_logits_concat)
+    prediction = post_processing_function_e(eval_examples, eval_dataset, outputs_numpy)
+
+    with open("result.csv", "w") as f:
+        f.write("id,answer\n")
+        for pair in prediction:
+            id_, text_ = pair["id"], pair["prediction_text"]
+            f.write(f"{id_},{text_}\n")
 
 if __name__ == "__main__":
     main()
