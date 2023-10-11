8f1e94119ad29b8c08e9d2a872631b9fee7f34e0
CHYang <chris920325@gmail.com>
2023-10-11 19:42:17 +0800

    Complete Package

--- hw1/download.sh
+++ hw1/download.sh
@@ -1,1 +1,5 @@
 #! /bin/bash
+
+# model datas
+gdown https://drive.google.com/drive/folders/1PlltWQWvU9QSWSdEuj09I3LYOOGDZ6VD -O ./model/multiple_choice_dir --folder
+gdown https://drive.google.com/drive/folders/1ugOlxEGIrCqOuYdGla6xFvMsnvLS4PuI -O ./model/extractive_dir --folder
